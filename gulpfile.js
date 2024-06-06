30579b42a09234480a056f8871f188853fc65789
iamcelestino <trcelestino488@gmail.com>
2024-06-06 15:32:02 -0700

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+  return src('public/**/*.scss')
+    .pipe(sass())
+    .pipe(dest('css'))
+}
+
+function watchTask() {
+  watch(['public/**/*scss'], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
--- src/App/Views/index.php
+++ src/App/Views/index.php
@@ -7,8 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <h1>HELOO MAMA, I am in the world</h1>
+<body class="">
+    <h1 class="font-bold text-2xl">HELOO MAMA, I am in the world</h1>
     <img src="<?php ROOT; ?>/images/map-65.svg" alt="">
+    <img src="<?php ROOT; ?>/images/customer-service-68.svg" alt="">
 </body>
 </html>
