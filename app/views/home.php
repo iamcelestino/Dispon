e20018b0ba0e82e3f637c4a08893c4afa3ba5b6a
iamcelestino <trcelestino488@gmail.com>
2024-06-09 13:06:22 -0700

    style: add style to home page

--- app/views/home.php
+++ app/views/home.php
@@ -5,18 +5,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="<?=ASSETS_URL?>css/index.css">
 </head>
 <body class="">
     <header class="">
         <div class="container">
-            <div class="">
+            <div class="navigation">
+                <div class="navigation_itens">
+                    <div class="logo">
+                        <h1><a href="">d<span class="is">is</span><span class="po">po</span>n</a></h1>
+                    </div>
+                   <nav class="">
+                        <ul class="nav__links">
+                            <li><a href="<?= BASE_URL ?>about">About</a></li>
+                            <li><a href="<?=  BASE_URL ?>contact">Contact</a></li>
+                        </ul>
+                   </nav>
+                </div>
+                <div class="nav__buttons">
+                    <li><a href="">Login</a></li>
+                    <li><a href="">Post a Product</a></li>
+                </div>
+            </div>
         </div>
     </header>
-    <h1 class="">HELOO MAMA, I am in the world</h1>
-    <a href="<?= BASE_URL ?>about">About</a>
-    <a href="<?=  BASE_URL ?>contact">Contact</a>
-    
-    <img src="<?=ASSETS_URL?>images/map-65.svg" alt="">
+    <main>
+        <section id="banner" >
+            <div class="container">
+                <div>
+                    <div class="">
+                        <img src="<?=ASSETS_URL?>images/map-65.svg" alt="">
+                    </div>
+                    <div class="">
+                        <h1>Find The <span>Perfect </span>Product for you</h1>
+                        <p>Find you products in hours not weeks, from you favorities suppliers, search for free</p>
+                        <form action="">
+                            <div>
+                                <input type="search" placeholder="Product name"><button type="submit">Search</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 
 </body>
 </html>
