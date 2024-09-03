f26782a66ab74975f9d59734ec4b21e3c0d73feb
iamcelestino <trcelestino488@gmail.com>
2024-09-03 09:36:56 -0700

    style: add style to footer

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,20 +1,20 @@
 <footer>
-        <div class="container">
+        <div class="container pt-16 pb-10">
             <div class="">
-                <div class="footer__item">
-                    <div class="logo">
-                        <ion-icon name="pie-chart"></ion-icon>
-                        <h1><a href="">d<span class="is">is</span><span class="po">po</span>n</a></h1>
-                    </div>
-                    <div>
-                        <ul class="nav__links">
-                            <li><a href="<?= BASE_URL ?>">About</a></li>
-                            <li><a href="<?= BASE_URL ?>">Contact</a></li>
-                        </ul>
+                <div class="flex justify-center">
+                    <div class="logo flex items-center">
+                        <ion-icon class="text-3xl" name="pie-chart"></ion-icon>
+                        <h1 class="font-bold text-3xl"><a href="">d<span class="is">is</span><span class="po">po</span>n</a></h1>
                     </div>
                 </div>
+                <div>
+                    <ul class="nav__links flex justify-center mt-4">
+                        <li class=""><a href="<?= BASE_URL ?>">About</a></li>
+                        <li class="ml-4"><a href="<?= BASE_URL ?>">Contact</a></li>
+                    </ul>
+                </div>
                 
-                <div class="footer__item">
+                <div class="md:flex justify-between">
                     <div class="">
                         <p> &copy;Copyright, Celestino Trosso</p>
                     </div>
